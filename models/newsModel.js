5c39b4246f7a382f87fd2264d2f1a573b082cbfc
bubita <bubfat@gmail.com>
2022-05-26 23:17:27 +0200

    Cambios tipos idUser

--- models/newsModel.js
+++ models/newsModel.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         idCreador: {
-            type: DataTypes.STRING,
+            type: DataTypes.INTEGER,
             allowNull: false
         },
         created_at : {
