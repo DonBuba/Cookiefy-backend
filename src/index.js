2f9c6b496d38dcdf86ab260a17c832e8a61eecf6
bubita <bubfat@gmail.com>
2022-05-27 09:13:44 +0200

    cambios

--- src/index.js
+++ src/index.js
@@ -20,6 +20,20 @@ const corsOpts = {
   };
 
 //Middlewares 
+app.use(function (req, res, next) {
+
+  var allowedDomains = ['https://cookiefy.vercel.app'];
+  var origin = req.headers.origin;
+  if(allowedDomains.indexOf(origin) > -1){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+})
 
 app.use(bodyParser.json())
 
