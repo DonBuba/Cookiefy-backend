1de6191a6da11c39dcb4e63d61979e2d462ce41c
bubita <bubfat@gmail.com>
2022-05-27 09:57:02 +0200

    Cors 2

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,21 +1,9 @@
 module.exports = {
-    // host: 'spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-    // user : 'xskzceutc80c6fsz',
-    // password: 'nxe1a0pdrpeb7lxi',
-    // db : 'zys4m51rb0q20mkr',
-    // port:'3306',
-    // dialect:'mysql',
-    // pool : {
-    //     max: 5,
-    //     min : 0,
-    //     acquire : 30000,
-    //     idle: 10000
-    // }
-    host: 'localhost',
-    user : 'root',
-    password: '',
-    db : 'cookiefy',
-    port:'8080',
+    host: 'spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    user : 'xskzceutc80c6fsz',
+    password: 'nxe1a0pdrpeb7lxi',
+    db : 'zys4m51rb0q20mkr',
+    port:'3306',
     dialect:'mysql',
     pool : {
         max: 5,
@@ -23,4 +11,16 @@ module.exports = {
         acquire : 30000,
         idle: 10000
     }
+    // host: 'localhost',
+    // user : 'root',
+    // password: '',
+    // db : 'cookiefy',
+    // port:'8080',
+    // dialect:'mysql',
+    // pool : {
+    //     max: 5,
+    //     min : 0,
+    //     acquire : 30000,
+    //     idle: 10000
+    // }
 }
